23c3287599264c27ca6748c780ee28c1753c5cb4
unknown <andrei.masala@yahoo.com>
2025-11-04 12:26:52 +0200

    First lab changed stem to plot and other small changes

--- Laboratorul1/main.py
+++ Laboratorul1/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # 1.
 # a) Simuleaza axa timpului printr-un sir de numere (il stochez in time_axis)
 time_axis = np.linspace(0, 0.03, num=int(0.03 / 0.0005))
@@ -27,7 +26,7 @@ plt.show()
 
 # c)
 fig2, axs2 = plt.subplots(3)
-interval = np.linspace(0,0.03,200)
+interval = np.linspace(0, 0.03, 200)
 axs2[0].set_xlabel("TImp (s)")
 axs2[1].set_xlabel("TImp (s)")
 axs2[2].set_xlabel("TImp (s)")
@@ -35,7 +34,7 @@ axs2[0].stem(interval, (np.cos(520 * np.pi * interval + np.pi / 3)))
 axs2[1].stem(interval, (np.cos(280 * np.pi * interval - np.pi / 3)))
 axs2[2].stem(interval, (np.cos(120 * np.pi * interval + np.pi / 3)))
 for ax in axs2:
-    ax.set_xlim(0,0.03)
+    ax.set_xlim(0, 0.03)
 plt.tight_layout()
 plt.show()
 
@@ -47,14 +46,14 @@ plt.show()
 
 # b)
 time_axis = np.linspace(0, 3, 3 * 200)
-plt.stem(time_axis, np.sin(2 * np.pi * time_axis * 800))
+plt.plot(time_axis, np.sin(2 * np.pi * time_axis * 800))
 plt.show()
 
 # c)
 freq_signal = 240
-duration = 4 / 240
-t = np.linspace(0, duration, 50)
-plt.stem(t, freq_signal * t - np.floor(freq_signal * t))
+duration = 1
+t = np.linspace(0, duration, 60)
+plt.plot(t, freq_signal * t - np.floor(freq_signal * t))
 plt.title("Semnal Sawtooth")
 plt.xlabel("Timp (s)")
 plt.ylabel("c[t]")
@@ -64,15 +63,15 @@ plt.show()
 # d)
 freq_signal = 300
 duration = 1.0
-t = np.linspace(0,duration,100)
-plt.stem(t, np.sign(np.sin(2 * np.pi * freq_signal * t)))
+t = np.linspace(0, duration, 100)
+plt.plot(t, np.sign(np.sin(2 * np.pi * freq_signal * t)))
 plt.title("Semnal Square")
 plt.xlabel("Timp(s)")
 plt.grid(True)
 plt.show()
 
 # e)
-I = np.random.rand(128,128)
+I = np.random.rand(128, 128)
 plt.imshow(I)
 plt.title("Semnal aleator 2D")
 plt.show()
@@ -89,7 +88,9 @@ plt.show()
 
 # 3
 # a)
-print("Pentru cu frecventa de esantionare de 2000 Hz , intervalul intre 2 esantioane este " + f"{1 / 2000: .4f}" + " secunde" )
+print(
+    "Pentru cu frecventa de esantionare de 2000 Hz , intervalul intre 2 esantioane este " + f"{1 / 2000: .4f}" + " secunde")
 
 # b)
-print("O ora de achizitie, pentru un esantion memorat pe 4 biti, la o frecv de esantionare de 2000 Hz, timp de o ora, va ocupa " + f"{4 * 2000 * 60 * 60 / 8 / 1024}" + " MB")
+print(
+    "O ora de achizitie, pentru un esantion memorat pe 4 biti, la o frecv de esantionare de 2000 Hz, timp de o ora, va ocupa " + f"{4 * 2000 * 60 * 60 / 8 / 1024}" + " MB")
