bcd2aa5ffec0637cee383fe898fc3176545a2cd2
Masala Andrei <andrei.masala@yahoo.com>
2025-11-08 17:24:37 +0200

    Lab 4

--- Laboratorul4/main.py
+++ Laboratorul4/main.py
@@ -3,6 +3,7 @@ import scipy
 import numpy as np
 import math
 import time
+from scipy.io import wavfile
 
 # N = [128, 256, 512, 1024, 2048, 4096] # 8192
 #
@@ -61,60 +62,60 @@ import time
 
 # 2
 # f < fs / 2, unde fs e frecv de esantionare, acum vreau ca fs / 2 < f
-fs = 20
-f = 8
-t = np.linspace(0, 1, fs, endpoint=False)
-t_good = np.linspace(0, 1, 10000, endpoint=False)
-
-signal_real = np.sin(2 * np.pi * f * t_good)
-signal_esant = np.sin(2 * np.pi * f * t)
-
-f1 = f + fs
-f2 = f + 2 * fs
-
-signal1_good = np.sin(2 * np.pi * f1 * t_good)
-signal1 = np.sin(2 * np.pi * f1 * t)
-
-signal2_good = np.sin(2 * np.pi * f2 * t_good)
-signal2 = np.sin(2 * np.pi * f2 * t)
-
-fig, axs = plt.subplots(3, figsize=(8, 6))
-axs[0].plot(t_good, signal_real)
-axs[0].plot(t, signal_esant, marker="o", linestyle="")
-axs[1].plot(t_good, signal1_good)
-axs[1].plot(t, signal1, marker="o", linestyle="")
-axs[2].plot(t_good, signal2_good)
-axs[2].plot(t, signal2, marker="o", linestyle="")
-plt.tight_layout()
-plt.show()
-
-# 3 fs / 2 > f
-fs = 200
-f = 8
-t = np.linspace(0, 1, fs, endpoint=False)
-t_good = np.linspace(0, 1, 10000, endpoint=False)
-
-signal_real = np.sin(2 * np.pi * f * t_good)
-signal_esant = np.sin(2 * np.pi * f * t)
-
-f1 = f + fs
-f2 = f + 2 * fs
-
-signal1_good = np.sin(2 * np.pi * f1 * t_good)
-signal1 = np.sin(2 * np.pi * f1 * t)
-
-signal2_good = np.sin(2 * np.pi * f2 * t_good)
-signal2 = np.sin(2 * np.pi * f2 * t)
-
-fig, axs = plt.subplots(3, figsize=(8, 6))
-axs[0].plot(t_good, signal_real)
-axs[0].plot(t, signal_esant, marker="o", linestyle="")
-axs[1].plot(t_good, signal1_good)
-axs[1].plot(t, signal1, marker="o", linestyle="")
-axs[2].plot(t_good, signal2_good)
-axs[2].plot(t, signal2, marker="o", linestyle="")
-plt.tight_layout()
-plt.show()
+# fs = 20
+# f = 8
+# t = np.linspace(0, 1, fs, endpoint=False)
+# t_good = np.linspace(0, 1, 10000, endpoint=False)
+#
+# signal_real = np.sin(2 * np.pi * f * t_good)
+# signal_esant = np.sin(2 * np.pi * f * t)
+#
+# f1 = f + fs
+# f2 = f + 2 * fs
+#
+# signal1_good = np.sin(2 * np.pi * f1 * t_good)
+# signal1 = np.sin(2 * np.pi * f1 * t)
+#
+# signal2_good = np.sin(2 * np.pi * f2 * t_good)
+# signal2 = np.sin(2 * np.pi * f2 * t)
+#
+# fig, axs = plt.subplots(3, figsize=(8, 6))
+# axs[0].plot(t_good, signal_real)
+# axs[0].plot(t, signal_esant, marker="o", linestyle="")
+# axs[1].plot(t_good, signal1_good)
+# axs[1].plot(t, signal1, marker="o", linestyle="")
+# axs[2].plot(t_good, signal2_good)
+# axs[2].plot(t, signal2, marker="o", linestyle="")
+# plt.tight_layout()
+# plt.show()
+#
+# # 3 fs / 2 > f
+# fs = 200
+# f = 8
+# t = np.linspace(0, 1, fs, endpoint=False)
+# t_good = np.linspace(0, 1, 10000, endpoint=False)
+#
+# signal_real = np.sin(2 * np.pi * f * t_good)
+# signal_esant = np.sin(2 * np.pi * f * t)
+#
+# f1 = f + fs
+# f2 = f + 2 * fs
+#
+# signal1_good = np.sin(2 * np.pi * f1 * t_good)
+# signal1 = np.sin(2 * np.pi * f1 * t)
+#
+# signal2_good = np.sin(2 * np.pi * f2 * t_good)
+# signal2 = np.sin(2 * np.pi * f2 * t)
+#
+# fig, axs = plt.subplots(3, figsize=(8, 6))
+# axs[0].plot(t_good, signal_real)
+# axs[0].plot(t, signal_esant, marker="o", linestyle="")
+# axs[1].plot(t_good, signal1_good)
+# axs[1].plot(t, signal1, marker="o", linestyle="")
+# axs[2].plot(t_good, signal2_good)
+# axs[2].plot(t, signal2, marker="o", linestyle="")
+# plt.tight_layout()
+# plt.show()
 # Se observa ca semnalul din prima figura este esantionat corect acum, dar pentru ca celelalte
 # sunt calculate in functie de fs, inca apare fenomenul de aliasing
 
@@ -127,10 +128,59 @@ plt.show()
 # Avand in vedere ca semnalul nostru nu este centrat in 0, lungimea de banda este de
 # 200 - 40 = 160 Hz
 # ceea ce inseamna ca
-# => fs >= 2B => fs >= 320 => fsmin = 160
+# => fs >= 2B => fs >= 320 => fsmin = 320
 # daca am fi aplicat criteriul Nyquist, fs ar fi trebuit sa fie 200 * 2 = 400H Hz (intrucat
 # cea mai mare frecventa ar fi fost 200), dar aici semnalul meu nu se centreaza in 0
 # asa ca de asta fac scaderea 200 - 40, ca sa vad care este B
 
 
 # 5
+# Vocalele a, e, i au o intensitate a culorii mai puternica (adica e amplitudinea sinusoidei mai mare)
+# de asemenea, vocala u este mai slaba si are frecventa mai joasa (coloana nu ajunge la fel de sus)
+
+# 6
+# sample_rate, signal = wavfile.read("vocale.wav")
+# #Pentru ca semnalul este stereo, selectez doar primul canal
+# if len(signal.shape) > 1:
+#     signal = signal[:, 0]
+#
+# N = len(signal)
+# group_dim = int(N * 1 / 100)
+# step = group_dim // 2
+# group_nr = (N - group_dim) // step + 1
+# spectrogram = []
+#
+# for i in range(group_nr):
+#     start = i * step
+#     end = start + group_dim
+#
+#     if end > N:
+#         break
+#
+#     group = signal[start:end]
+#     fft_group = np.fft.fft(group)
+#     spectrogram.append(np.abs(fft_group))
+#
+# spectrogram = np.array(spectrogram).T # ca sa am frecventa pe oriz
+# spectrogram = spectrogram[:group_dim//2, :]
+# spectrogram_db = 20 * np.log10(spectrogram + 1e-10)
+#
+# t = np.linspace(0, N / sample_rate, group_nr)
+# freq = np.linspace(0, sample_rate / 2, group_dim // 2)
+#
+# plt.pcolormesh(t, freq, spectrogram_db, shading='gouraud', cmap='plasma')
+#
+# plt.xlabel('Timp (s)')
+# plt.ylabel('Frecvență (Hz)')
+# plt.colorbar(label='Magnitudine (dB)')
+# plt.show()
+#daca inchizi un ochi si inclini capul seamana putin cu ala din Audacity
+
+# 7
+p_signal_db = 90
+snr_db = 80
+
+# SNR = Psemnal / Pzgomot
+# SNRdb = 10log10 SNR
+# SNRdb = Psemnaldb - Pzgomotdb
+print("Puterea zgomotului este de " + str(p_signal_db - snr_db) + " decibeli")
