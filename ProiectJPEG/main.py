7115492db784f9d37b20b4f5593966a9aeec74f3
unknown <andrei.masala@yahoo.com>
2025-12-30 09:35:55 +0200

    Modifications project

--- ProiectJPEG/main.py
+++ ProiectJPEG/main.py
@@ -1,7 +1,6 @@
 import pickle
 import struct
 from collections import Counter
-
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot
@@ -12,12 +11,6 @@ import cv2
 from concurrent.futures import ThreadPoolExecutor
 import heapq
 import sys
-
-# def huffman_coding(option):
-#     if option == 1:
-#
-#     else:
-
 
 Q_jpeg = [[16, 11, 10, 16, 24, 40, 51, 61],
           [12, 12, 14, 19, 26, 28, 60, 55],
@@ -35,10 +28,10 @@ zigzag_indices = [
     21, 34, 37, 47, 50, 56, 59, 61, 35, 36, 48, 49, 57, 58, 62, 63
 ]
 
-# https://www.geeksforgeeks.org/dsa/huffman-coding-in-python/
 sys.setrecursionlimit(2000)
 
 
+# https://www.geeksforgeeks.org/dsa/huffman-coding-in-python/ de unde m am inspirat
 class HuffmanNode:
     def __init__(self, freq, symbol=None):
         self.freq = freq
@@ -79,70 +72,11 @@ def build_huff_codes(node, code="", huffman_codes=None):
     return huffman_codes
 
 
-# Fac conversie ycbcr pt ca ochiul uman percepe mai bine intensitatea
-# luminii decat culori in sine si e mai usor de comprimat
-
-img = matplotlib.image.imread("sample3.bmp")
-
-# Vad daca imaginea se poate imparti in blocuri de 8x8, daca nu ii fac padding cu 0
-print(img[:, :, 0].shape)
-h, w, _ = img.shape
-pad_h = (8 - (h % 8)) % 8
-pad_w = (8 - (w % 8)) % 8
-img = np.pad(img, ((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
-
-imgYCC = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-h, w, _ = img.shape
-B = np.zeros_like(imgYCC, dtype=np.float32)
-A = np.zeros_like(imgYCC, dtype=np.float32)
-
-
-def worker(i):
-    for j in range(0, w, 8):
-        for c in range(3):
-            x = imgYCC[i:i + 8, j:j + 8, c]
-            # Scot media din semnal
-            mean = np.mean(x)
-            x = x - mean
-            means[i // 8, j // 8, c] = mean
-            b = dctn(x, norm='ortho')
-            b_jpeg = np.round(b / Q_jpeg)
-            temp = b_jpeg * Q_jpeg
-            temp = idctn(temp, norm='ortho')
-            temp = temp + mean
-            A[i:i + 8, j:j + 8, c] = temp
-            B[i:i + 8, j:j + 8, c] = b_jpeg
-
-
-# M am enervat ca era f slow pe o poza f mare, asa ca am incercat sa fac paralel dar nu merge cu MULT mai bine
-means = np.zeros((h // 8, w // 8, 3))  # salvez mediile pt decompresie
-with ThreadPoolExecutor(max_workers=24) as executor:
-    for i in range(0, h, 8):
-        executor.submit(worker, i)
-
-all_values_to_encode = []
-# Comprimam
-for i in range(0, h, 8):
-    for j in range(0, w, 8):
-        for c in range(3):
-            bloc = B[i:i + 8, j:j + 8, c]
-            flat_block = bloc.flatten()
-            zigzag_vector = [int(flat_block[idx]) for idx in zigzag_indices]
-            all_values_to_encode.extend(zigzag_vector)
-
-x = Counter(all_values_to_encode)
-symbols = list(x.keys())
-freqs = list(x.values())
-root = build_huff_tree(symbols, freqs)
-huffman_dict = build_huff_codes(root, "", None)
-print(huffman_dict)
-
-
 def pack_to_binary(all_values, huff_dict):
     bit_string = "".join(huff_dict[val] for val in all_values)
 
-    # Calculez padding-ul (bitii extra pana la un octet complet)
-    padding_len = (8 - len(bit_string) % 8) % 8
+    # Calculez padding-ul pana la urm byte
+    padding_len = 8 - (len(bit_string) % 8)
     bit_string += "0" * padding_len
 
     byte_arr = bytearray()
@@ -152,31 +86,149 @@ def pack_to_binary(all_values, huff_dict):
     return bytes(byte_arr), padding_len
 
 
-binary_data, padding = pack_to_binary(all_values_to_encode, huffman_dict)
-l = []
-for i in huffman_dict.keys():
-    l.append((i, huffman_dict[i]))
-print(l)
-
-with open("compressed_image.bin", "wb") as f:
-    # Salvam Header-ul: Latime(I), Inaltime(I), Padding(B) -> 9 octeți
-    f.write(struct.pack('IIB', w, h, padding))
-
-    # Salvam dicționarul (serializat)
-    dict_bytes = pickle.dumps(l)
-    f.write(struct.pack('I', len(l)))  # Scriem lungimea dicționarului
-    f.write(dict_bytes)
-
-    # Scriem bitii imaginii
-    f.write(binary_data)
 
 
+def comprimareJPEG(path):
+    img = matplotlib.image.imread(path)
+
+    def worker(i):
+        for j in range(0, w, 8):
+            for c in range(3):
+                x = imgYCC[i:i + 8, j:j + 8, c]
+                # Scot media din semnal
+                mean = np.mean(x)
+                x = x - mean
+                means[i // 8, j // 8, c] = mean
+                b = dctn(x, norm='ortho')
+                b_jpeg = np.round(b / Q_jpeg)
+                B[i:i + 8, j:j + 8, c] = b_jpeg
+
+    # Vad daca imaginea se poate imparti in blocuri de 8x8, daca nu ii fac padding cu 0
+    print(img[:, :, 0].shape)
+    h, w, _ = img.shape
+    pad_h = (8 - (h % 8)) % 8
+    pad_w = (8 - (w % 8)) % 8
+    img = np.pad(img, ((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
+
+    # Fac conversie ycbcr pt ca ochiul uman percepe mai bine intensitatea
+    # luminii decat culori in sine si e mai usor de comprimat
+    imgYCC = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    h, w, _ = img.shape
+    B = np.zeros_like(imgYCC, dtype=np.float32)
+    # M am enervat ca era f slow pe o poza f mare, asa ca am incercat sa fac paralel dar nu merge cu MULT mai bine
+    means = np.zeros((h // 8, w // 8, 3))  # salvez mediile pt decompresie
+    with ThreadPoolExecutor(max_workers=48) as executor:
+        for i in range(0, h, 8):
+            executor.submit(worker, i)
+
+    all_values_to_encode = []
+    # Comprimam
+    for i in range(0, h, 8):
+        for j in range(0, w, 8):
+            for c in range(3):
+                bloc = B[i:i + 8, j:j + 8, c]
+                flat_block = bloc.flatten()
+                zigzag_vector = [int(flat_block[idx]) for idx in zigzag_indices]
+                all_values_to_encode.extend(zigzag_vector)
+
+    x = Counter(all_values_to_encode)
+    symbols = list(x.keys())
+    freqs = list(x.values())
+    root = build_huff_tree(symbols, freqs)
+    huffman_dict = build_huff_codes(root, "", None)
+    print(huffman_dict)
+
+    binary_data, padding = pack_to_binary(all_values_to_encode, huffman_dict)
+
+    with open("compressed_image.bin", "wb") as f:
+        f.write(struct.pack('IIB', w, h, padding))
+
+        dict_bytes = pickle.dumps(huffman_dict)
+        dict_size_bytes = len(dict_bytes)
+
+        f.write(struct.pack('I', dict_size_bytes))
+        f.write(dict_bytes)
+
+        f.write(binary_data)
+
+    return means
+
+
+def decomprimare_jpeg(path, means):
+    with open(path, "rb") as f:
+        w, h, padding = struct.unpack('IIB', f.read(9))
+
+        dict_size_bytes = struct.unpack('I', f.read(4))[0]
+
+        dict_data = f.read(dict_size_bytes)
+        huff_dict = pickle.loads(dict_data)
+
+        binary_data = f.read()
+
+    bit_string = "".join(f"{byte:08b}" for byte in binary_data)
+    if padding > 0:
+        bit_string = bit_string[:-padding]
+
+    reverse_huff_dict = {v: k for k, v in huff_dict.items()}
+
+    decoded_values = []
+    current_code = ""
+
+    for bit in bit_string:
+        current_code += bit
+        if current_code in reverse_huff_dict:
+            decoded_values.append(reverse_huff_dict[current_code])
+            current_code = ""
+
+    img_reconstructed = np.zeros((h, w, 3), dtype=np.float32)
+
+    def inverse_zigzag(flat_vector_64):
+        block_flat = np.zeros(64, dtype=np.float32)
+        for i, idx in enumerate(zigzag_indices):
+            block_flat[idx] = flat_vector_64[i]
+        return block_flat.reshape((8, 8))
+
+    idx_counter = 0
+    B_recovered = np.zeros((h, w, 3), dtype=np.float32)
+
+    for i in range(0, h, 8):
+        for j in range(0, w, 8):
+            for c in range(3):
+                vector_zigzag = decoded_values[idx_counter: idx_counter + 64]
+                idx_counter += 64
+
+                block_quantized = inverse_zigzag(vector_zigzag)
+                B_recovered[i:i + 8, j:j + 8, c] = block_quantized
+
+    def worker_decomp(i):
+        for j in range(0, w, 8):
+            for c in range(3):
+                b_jpeg = B_recovered[i:i + 8, j:j + 8, c]
+
+                b = b_jpeg * Q_jpeg
+
+                x_reconst = idctn(b, norm='ortho')
+                x_final = x_reconst + means[i // 8, j // 8, c]
+
+                img_reconstructed[i:i + 8, j:j + 8, c] = x_final
+
+    with ThreadPoolExecutor(max_workers=48) as executor:
+        for i in range(0, h, 8):
+            executor.submit(worker_decomp, i)
+
+    img_final_uint8 = np.clip(img_reconstructed, 0, 255).astype(np.uint8)
+
+    img_rgb = cv2.cvtColor(img_final_uint8, cv2.COLOR_YCrCb2RGB)
+
+    return img_rgb
+
+
+path = "sample3.bmp"
+means = comprimareJPEG(path)
+img_rgb = decomprimare_jpeg("compressed_image.bin", means)
 fig, ax = plt.subplots(1, 2, figsize=(15, 7))
-A_clipped = np.clip(A, 0, 255)
-A_final = A_clipped.astype(np.uint8)
-A_final = cv2.cvtColor(A_final, cv2.COLOR_YCrCb2RGB)
-# y_jpeg = idctn(B)
-ax[0].imshow(A_final)
+img = matplotlib.image.imread(path)
+ax[0].imshow(img_rgb)
 ax[0].set_title("Fake JPEG Compressed")
 ax[1].imshow(img)
 ax[1].set_title("Original")
